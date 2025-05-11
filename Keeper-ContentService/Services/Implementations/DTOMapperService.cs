fb4974a55176600582b5067730868b6a5626515b
9im0n <demanekeforov2006@gmail.com>
2025-05-11 18:07:51 +0300

    DTO mapper that can map entities from DB to DTO.

---
+++ Keeper-ContentService/Services/Implementations/DTOMapperService.cs
@@ -0,0 +1,80 @@
+﻿using Keeper_ContentService.Models.Db;
+using Keeper_ContentService.Models.DTO;
+using Keeper_ContentService.Services.Interfaces;
+
+namespace Keeper_ContentService.Services.Implementations
+{
+    public class DTOMapperService : IDTOMapper
+    {
+        public CategoryDTO Map(Category category)
+        {
+            return new CategoryDTO()
+            {
+                Id = category.Id,
+                Name = category.Name,
+            };
+        }
+
+        public ArticleStatusDTO Map(ArticleStatus status)
+        {
+            return new ArticleStatusDTO()
+            {
+                Id = status.Id,
+                Name = status.Name,
+            };
+        }
+
+        public CommentDTO Map(Comment comment)
+        {
+            return new CommentDTO()
+            {
+                Id = comment.Id,
+                ArticleId = comment.ArticleId,
+                AuthorId = comment.AuthorId,
+                ParentCommentId = comment.CommentId,
+                Text = comment.Text,
+            };
+        }
+
+        public ArticleDTO Map(Article article)
+        {
+            return new ArticleDTO()
+            {
+                Id = article.Id,
+                Category = Map(article.Category),
+                AuthorId = article.AuthorId,
+                Satus = Map(article.Status),
+                Title = article.Title,
+                Content = article.Content,
+                Comments = Map(article.Comments)
+            };
+        }
+
+        public LikedArticleDTO Map(LikedArticle likedArticle)
+        {
+            return new LikedArticleDTO()
+            {
+                Id = likedArticle.Id,
+                UserId = likedArticle.UserId,
+                ArticleId = likedArticle.ArticleId
+            };
+        }
+
+        public SavedArticleDTO Map(SavedArticle savedArticle)
+        {
+            return new SavedArticleDTO()
+            {
+                Id = savedArticle.Id,
+                UserId = savedArticle.UserId,
+                ArticleId = savedArticle.ArticleId
+            };
+        }
+
+        public ICollection<CategoryDTO> Map(ICollection<Category> categories) => categories.Select(Map).ToList();
+        public ICollection<ArticleStatusDTO> Map(ICollection<ArticleStatus> articleStatuses) => articleStatuses.Select(Map).ToList();
+        public ICollection<CommentDTO> Map(ICollection<Comment> comments) => comments.Select(Map).ToList();
+        public ICollection<ArticleDTO> Map(ICollection<Article> articles) => articles.Select(Map).ToList();
+        public ICollection<LikedArticleDTO> Map(ICollection<LikedArticle> likedArticles) => likedArticles.Select(Map).ToList();
+        public ICollection<SavedArticleDTO> Map(ICollection<SavedArticle> savedArticles) => savedArticles.Select(Map).ToList();
+    }
+}
---
+++ Keeper-ContentService/Services/Interfaces/IDTOMapper.cs
@@ -0,0 +1,24 @@
+﻿using Keeper_ContentService.Models.Db;
+using Keeper_ContentService.Models.DTO;
+
+namespace Keeper_ContentService.Services.Interfaces
+{
+    public interface IDTOMapper
+    {
+        // Single
+        public CategoryDTO Map(Category category);
+        public ArticleStatusDTO Map(ArticleStatus status);
+        public CommentDTO Map(Comment comment);
+        public ArticleDTO Map(Article article);
+        public LikedArticleDTO Map(LikedArticle likedArticle);
+        public SavedArticleDTO Map(SavedArticle savedArticle);
+
+        // Collection
+        public ICollection<CategoryDTO> Map(ICollection<Category> categories);
+        public ICollection<ArticleStatusDTO> Map(ICollection<ArticleStatus> articleStatuses);
+        public ICollection<CommentDTO> Map(ICollection<Comment> comments);
+        public ICollection<ArticleDTO> Map(ICollection<Article> articles);
+        public ICollection<LikedArticleDTO> Map(ICollection<LikedArticle> likedArticles);
+        public ICollection<SavedArticleDTO> Map(ICollection<SavedArticle> savedArticles);
+    }
+}
