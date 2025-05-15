e6fd8ef0de95ab210138a86ed285b5e6b9a0447c
9im0n <demanekeforov2006@gmail.com>
2025-05-15 11:27:37 +0300

    get liked articles

---
+++ Keeper-ContentService/Controllers/LikedArticlesController.cs
@@ -0,0 +1,40 @@
+﻿using Keeper_ContentService.Models.DTO;
+using Keeper_ContentService.Models.Service;
+using Keeper_ContentService.Services.Interfaces;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Keeper_ContentService.Controllers
+{
+    [ApiController]
+    [Route("liked-articles")]
+    public class LikedArticlesController : ControllerBase
+    {
+        private ILikedArticlesService _service;
+
+        public LikedArticlesController(ILikedArticlesService service)
+        {
+            _service = service;
+        }
+
+
+
+        [HttpGet]
+        [Authorize]
+        public async Task<IActionResult> 
+            GetPagedLikedArticles([FromQuery] PagedRequestDTO<LikedArticlesFillterDTO> request)
+        {
+            ServiceResponse<PagedResultDTO<LikedArticleDTO>?> response = await _service.GetPaginationAsync(request, User);
+            return HandleServiceResponse(response);
+        }
+
+
+        private IActionResult HandleServiceResponse<T>(ServiceResponse<T> response)
+        {
+            if (!response.IsSuccess)
+                return StatusCode(statusCode: response.Status, new { message = response.Message });
+
+            return StatusCode(statusCode: response.Status, new { data = response.Data, message = response.Message });
+        }
+    }
+}
